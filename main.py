36aee9522485c511f226b6049ef8d18fc360d801
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-15 22:01:13 -0400

    The connection can be established!

---
+++ main.py
@@ -0,0 +1,9 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+
+if __name__ == "__main__":
+    mongodb_client = MongoDBClient()
+    print("Collection Name:", mongodb_client.database.list_collection_names())
+
+
+
+
