23f09bf452f954333c36744b223bed9bfd3cc6dd
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-16 21:45:01 -0400

    Edits to exception.py code

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os, sys
+
+def test_exception():
+    try:
+        x = 1/0
+    except Exception as e:
+        raise SensorException(e, sys)
 
 if __name__ == "__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
     mongodb_client = MongoDBClient()
     print("Collection Name:", mongodb_client.database.list_collection_names())
 
