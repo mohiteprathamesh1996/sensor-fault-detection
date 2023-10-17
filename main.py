734da2f833849adf06c274207d472427f76af697
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-17 15:01:13 -0400

    Setting up data ingestion pipeline

--- main.py
+++ main.py
@@ -2,23 +2,10 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os, sys
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 
-def test_exception():
-    try:
-        x = 1/0
-    except Exception as e:
-        raise SensorException(e, sys)
 
 if __name__ == "__main__":
-    try:
-        logging.info("We are dividing 1 by 0")
-        test_exception()
-    except Exception as e:
-        print(e)
-
-    mongodb_client = MongoDBClient()
-    print("Collection Name:", mongodb_client.database.list_collection_names())
-
-
-
-
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -4,7 +4,7 @@ from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 # Define common constant variables for training pipeline
 TARGET_COLUMN = "class"
 PIPELINE_NAME = "sensor"
-ARTIFICAT_DIR = "artifact"
+ARTIFACT_DIR = "artifact"
 FILE_NAME = "sensor.csv"
 
 TRAIN_FILE_NAME = "train.csv"
@@ -15,13 +15,11 @@ MODEL_FILE_NAME = "model.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
 
-"""
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
-"""
-DATA_INGESTION_COLLECTION_NAME: str = "car"
-DATA_INGESTION_DIR_NAME: str = "data_ingestion"
-DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-DATA_INGESTION_INGESTED_DIR: str = "ingested"
+# Define constants for data ingestion
+DATA_INGESTION_COLLECTION_NAME = "car"
+DATA_INGESTION_DIR_NAME = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
+DATA_INGESTION_INGESTED_DIR = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
 
