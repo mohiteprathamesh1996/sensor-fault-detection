683a551c438d20ef0eb65c8474216f2791c5c1d4
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-18 14:27:41 -0400

    Data Ingestion artifact was created!

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os, sys
 from sensor.pipeline.training_pipeline import TrainPipeline
 
 
+
 if __name__ == "__main__":
     train_pipeline = TrainPipeline()
     train_pipeline.run_pipeline()
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -2,21 +2,25 @@ from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionCon
 from sensor.exception import SensorException
 from sensor.logger import logging
 from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.component.data_ingestion import DataIngestion
 import os, sys
 
 class TrainPipeline:
 
     def __init__(self):
         training_pipeline_config = TrainingPipelineConfig()
-        self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         self.training_pipeline_config = training_pipeline_config
         
 
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Starting data ingestion")
-            logging.info("Data ingestion completed")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
 
         except  Exception as e:
             raise  SensorException(e, sys)
@@ -53,6 +57,6 @@ class TrainPipeline:
 
     def run_pipeline(self):
         try:
-            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_ingestion_artifact = self.start_data_ingestion()
         except Exception as e:
             raise SensorException(e, sys)
