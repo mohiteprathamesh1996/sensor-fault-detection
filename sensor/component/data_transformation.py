807e1d064683a025f1645d82cadbefb32c5eb428
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-22 08:58:20 -0400

    Data transformation completed!!!

--- sensor/component/data_transformation.py
+++ sensor/component/data_transformation.py
@@ -98,8 +98,7 @@ class DataTransformation:
             save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
             save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
             save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_object,)
-            
-            
+              
             #preparing artifact
             data_transformation_artifact = DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
