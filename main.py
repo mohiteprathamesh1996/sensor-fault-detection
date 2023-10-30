ae97bf1286eb3b943fca26b8960015ee3fa9f3ca
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-29 21:20:03 -0400

    Changes - 2

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ env_file_path=os.path.join(os.getcwd(), "env.yaml")
 
 def set_env_variable(env_file_path):
 
-    if os.getenv('MONGO_DB_URL', None) is None:
+    if os.getenv('MONGODB_URL', None) is None:
         env_config = read_yaml_file(env_file_path)
-        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
+        os.environ['MONGODB_URL']=env_config['MONGO_DB_URL']
 
 
 app = FastAPI()
