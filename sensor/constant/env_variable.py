78f391f7a5e40d6d6bbc44929e6e7f27724d6436
Prathamesh Mohite <59126968+mohiteprathamesh1996@users.noreply.github.com>
2023-10-29 21:30:03 -0400

    Update env_variable.py

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -1,5 +1,6 @@
 import os
-MONGODB_URL = os.getenv('MONGODB_URL')
+# MONGODB_URL = os.getenv('MONGODB_URL')
+MONGODB_URL= "mongodb+srv://prathameshmohite96:Psm%4020696@clusterpm.jycq9ph.mongodb.net/?retryWrites=true&w=majority"
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
