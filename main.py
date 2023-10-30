8681bed8c6c2255d54199c97b095059b653e4bc5
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-10-29 21:10:45 -0400

    Changes

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ from sensor.utils.main_utils import load_object
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-env_file_path=os.path.join(os.getcwd(),"env.yaml")
+env_file_path=os.path.join(os.getcwd(), "env.yaml")
 
 def set_env_variable(env_file_path):
 
-    if os.getenv('MONGO_DB_URL',None) is None:
+    if os.getenv('MONGO_DB_URL', None) is None:
         env_config = read_yaml_file(env_file_path)
         os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
@@ -78,7 +78,7 @@ async def predict_route():
 
 def main():
     try:
-#        set_env_variable(env_file_path)
+        set_env_variable(env_file_path)
         training_pipeline = TrainPipeline()
         training_pipeline.run_pipeline()
     except Exception as e:
